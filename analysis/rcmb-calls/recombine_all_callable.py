35193868de796a3bb78a8cd38d6a4931795e526f
Ahmed Hasan <ahmed.hasan@mail.utoronto.ca>
2019-08-13 14:15:41 -0400

    first pass at recombine all callable script

---
+++ analysis/rcmb-calls/recombine_all_callable.py
@@ -0,0 +1,154 @@
+'''
+recombine_all_callable.py - 'simulate' recombination events
+with all callable sites
+
+takes in a space-separated file that contains desired locations of recombination events:
+
+type position length_bp
+CO 200 0
+CO-GC 800 40
+NCO 500 100
+
+corresponds to simple CO at 200 bp, CO-GC at 800 bp with 40 bp GC,
+and NCO at 500 bp of length 100 bp
+
+parent 1 will be all 'A' and parent 2 all 'T'
+'''
+
+import argparse
+from tqdm import tqdm
+import random
+import numpy as np
+from copy import deepcopy
+
+def args():
+    parser = argparse.ArgumentParser(
+        description='', 
+        usage='python3.5 script.py [options]')
+
+    parser.add_argument('-f', '--fname', required=True,
+                        type=str, help='File containing recombination events')
+    parser.add_argument('-l', '--length', required=True,
+                        type=str, help='Length of parental haps')
+    parser.add_argument('-o', '--out', required=True,
+                        type=str, help='File to write to')
+
+    args = parser.parse_args()
+
+    return args.fname, args.length, args.out
+
+
+def parse_events(fname: str) -> dict:
+    '''
+    parses input file containing recombination events
+    should be a space separated file with columns event, position, and length
+    '''
+    events = {}
+    with open(fname, 'r') as f:
+        for line in f:
+            if not line.startswith('event'):
+                event, position, length_bp = line.split(' ')
+                events[position] = [event, length_bp]
+    return events
+
+def draw_from_hap(hap_in: np.ndarray, event: str):
+    '''
+    helper function for recombine() that takes in an input
+    hap and event and alters genotypes
+    if CO, will return single hap (since switch point is instant)
+    but for NCO/CO-GC, will return two (at start of event + at the
+    end of the tract)
+    '''
+    hap = deepcopy(hap_in)
+    if event == 'CO':
+        # draw haps to switch
+        parent1, parent2 = np.where(hap == 'A'), np.where(hap == 'T')
+        switch1, switch2 = random.choice(*parent1), random.choice(*parent2)
+        # switch phases
+        hap[switch1] = 'T'
+        hap[switch2] = 'A'
+        return hap
+    elif event == 'NCO': # this is wrong - manually check it
+        hap2 = deepcopy(hap)
+        to_switch = random.choice([0,1,2,3])
+        if hap[to_switch] == 'A':
+            hap[to_switch] = 'T'
+        elif hap[to_switch] == 'T':
+            hap[to_switch] = 'A'
+        return hap, hap2
+    elif event == 'CO-GC':
+        # determine phase switches
+        parent1, parent2 = np.where(hap == 'A'), np.where(hap == 'T')
+        switch1, switch2 = random.choice(*parent1), random.choice(*parent2)
+        hap2 = deepcopy(hap)
+        # create GC tract
+        to_switch = random.choice([switch1, switch2])
+        if hap[to_switch] == 'A':
+            hap[to_switch] = 'T'
+        elif hap[to_switch] == 'T':
+            hap[to_switch] = 'A'
+        # switch phases after GC tract
+        hap2[switch1] = 'T'
+        hap2[switch2] = 'A'
+        return hap, hap2
+
+        
+def recombine(input_events: dict) -> dict:
+    '''
+    uses input event file to record recombination events
+    assuming parents are of hap A or hap T
+    '''
+    initial_hap = ['A', 'A', 'T', 'T']
+    phase_tracker = {0: np.array(initial_hap)}
+    for position in tqdm(sorted(input_events.keys())):
+        # convert existing positions to np array
+        current_positions = np.array(sorted(list(phase_tracker.keys())))
+        # get hap at highest position < position of event
+        max_pos = current_positions[np.where(current_positions < position)[0].max()]
+        current_hap = phase_tracker[max_pos]
+        event, length_bp = input_events[position]
+        if event == 'CO':
+            out_hap = draw_from_hap(current_hap, 'CO')
+            phase_tracker[position] = out_hap
+        elif event in ['NCO', 'CO-GC']:
+            out_hap1, out_hap2 = draw_from_hap(current_hap, event)
+            phase_tracker[position] = out_hap1
+            phase_tracker[position + length_bp] = out_hap2
+
+    return phase_tracker
+            
+def write_recombinants(phase_changes: dict, length: int, out: str):
+    '''
+    uses event dict output by recombine() to create output
+    sequences in fasta format
+    '''
+    change_positions = sorted(phase_changes.keys())
+    with open(out, 'w') as f:
+        for hap in tqdm(range(3)):
+            f.write('>{hap}'.format(hap=hap) + '\n')
+            sequence = ''
+            for i in range(len(change_positions) - 1):
+                position = change_positions[i]
+                next_position = change_positions[i + 1]
+                current_base = phase_changes[position][hap]
+                sequence += ''.join([current_base for i in range(next_position - position)])
+            remaining = length - len(sequence)
+            last_base = sequence[-1]
+            sequence += ''.join([last_base for i in range(remaining)])
+            f.write(sequence + '\n')
+            
+def main():
+    fname, length, out = args()
+    print('Parsing input file...')
+    input_events = parse_events(fname)
+    print('Determining phase changes...')
+    phase_changes = recombine(input_events)
+    print('Creating sequences and writing to file...')
+    write_recombinants(phase_changes)
+    print('Done.')
+    print('Good job!')
+
+if __name__ == '__main__':
+    main()
+
+        
--- analysis/tetrad-draws/log.md
+++ analysis/tetrad-draws/log.md
@@ -155,9 +155,61 @@ to do:
     - also simulate data where this is the case?
     - could also use AT or 12 system to designate parental haps - probably easier
 
+```bash
+mkdir -p analysis/rcmb-calls
+mkdir -p data/rcmb-calls
+```
 
+creating 'all callable' sites:
+- create two 10 kb fasta records
+    - parent 1 - all As
+    - parent 2 - all Ts
+- create simulated 'tetrad' from cross b/w parent 1 and 2
+    - walk along chromosome and determine crossover/NCO locations using Poisson process
+    - update four tetrad strings accordingly
+    - could be faster if done in 'long' format before transposing?
+- do 'read draws' from single individuals in tetrad
+    - what SNP combinations indicate one form of recombination over another?
+    
 
+creating two 10 kb 'parents':
 
+```python
+>>> outname = 'data/rcmb-calls/parents_all_callable.fasta'
+>>> d = dict.fromkeys(['parent1', 'parent2'], '')
+>>> d['parent1'] = ''.join(['A' for i in range(10000)])
+>>> d['parent2'] = ''.join(['T' for i in range(10000)])
+>>> with open(outname, 'w') as f:
+  2     for id, seq in d.items():
+  3         f.write('>' + id + '\n')
+  4         f.write(seq + '\n')
+```
+
+## 9/8/2019
+
+'recombining' the parents
+
+some fixed rcmb events -
+- COs
+    - 200 kb
+    - 800 kb - this could be CO-GC
+- NCOs
+    - 500 kb - 100 bp long
+    - 700 kb - 50 bp long
+
+script should encode 'instructions' for where phase changes happen instead
+of storing large strings in memory
+
+at start - create four 'offspring' - assign 2 to parent 1 (A) and
+2 to parent 2 (T)
+
+- if CO
+    - draw one 'A' offspring and one 'T' offspring and switch to other phase
+- if CO-GC
+    - encode phase change _n_ bases downstream of initial 'switch point' _s_
+    - in distance b/w _s_ and _n_ draw one offspring and switch phase for that length
+- if NCO-GC
+    - draw one offspring and temporarily switch phase for _n_ bases
 
 
 
